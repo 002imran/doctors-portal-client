dca953a95ca6e27a559b30cecf39c0fcd24f5b2b
alimranhossain <alimranhossain44@gmail.com>
2022-11-12 19:59:31 +0600

    dynamic info card section

---
+++ src/Pages/Home/InfoCards/InfoCard.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const InfoCard = ({card}) => {
+    const {icon,name, description, bgClass } = card;
+    return (
+      <div className={`card card-side bg-base-100 shadow-xl p-6 ${bgClass}`}>
+        <figure>
+          <img src={icon} alt="Movie" />
+        </figure>
+        <div className="card-body">
+          <h2 className="card-title">{name}</h2>
+          <p>{description}</p>
+        
+        </div>
+      </div>
+    );
+};
+
+export default InfoCard;
