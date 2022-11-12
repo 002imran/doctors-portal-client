013f1a0c3f69e537ac1421d774835be8a77c3ff8
alimranhossain <alimranhossain44@gmail.com>
2022-11-12 19:08:33 +0600

    banner section

--- src/Pages/Shared/Navbar/Navbar.js
+++ src/Pages/Shared/Navbar/Navbar.js
@@ -13,8 +13,8 @@ const Navbar = () => {
     );
 
     return (
-      <div>
-        <div className="navbar bg-base-100">
+    
+        <div className="navbar bg-base-100 justify-between">
           <div className="navbar-start">
             <div className="dropdown">
               <label tabIndex={0} className="btn btn-ghost lg:hidden">
@@ -49,7 +49,7 @@ const Navbar = () => {
           </div>
          
         </div>
-      </div>
+ 
     );
 };
 
