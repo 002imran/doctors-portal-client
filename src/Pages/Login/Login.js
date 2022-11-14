332baacb8dfd70c70c9afe7713fb2149b0d0732f
alimranhossain <alimranhossain44@gmail.com>
2022-11-14 15:33:27 +0600

    login form validation

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -4,7 +4,12 @@ import { Link } from 'react-router-dom';
 
 
 const Login = () => {
-    const { register, handleSubmit} = useForm();
+    const {
+      register,
+      formState: { errors },
+      handleSubmit,
+    } = useForm();
+
     const handleLogin = data =>{
         console.log(data);
     }
@@ -12,31 +17,41 @@ const Login = () => {
       <div className="h-[800px] flex justify-center items-center">
         <div className="w-96 p-7">
           <h2 className="text-4xl text-center">Login</h2>
-          <form
-            onSubmit={handleSubmit(handleLogin)}
-          >
+          <form onSubmit={handleSubmit(handleLogin)}>
             <div className="form-control w-full max-w-xs">
               <label className="label">
                 <span className="label-text">Email</span>
               </label>
               <input
                 type="email"
-                {...register("email")}
+                {...register("email", {
+                  required: "Email is required",
+                })}
                 placeholder=""
                 className="input input-bordered w-full max-w-xs"
               />
+              {errors.email && (
+                <p className="text-red-600">{errors.email?.message}</p>
+              )}
               <label className="label">
                 <span className="label-text">Password</span>
               </label>
+
               <input
                 type="password"
-                {...register("passqord")}
-                placeholder=""
+                {...register("password", {
+                  required: "Password is required",
+                  minLength: {value: 6, message: "Password must be 6 characters or longer"}
+                })}
                 className="input input-bordered w-full max-w-xs"
               />
+
               <label className="label">
                 <span className="label-text">Forget Password?</span>
               </label>
+              {errors.password && (
+                <p className="text-red-600">{errors.password?.message}</p>
+              )}
             </div>
             <input
               type="submit"
