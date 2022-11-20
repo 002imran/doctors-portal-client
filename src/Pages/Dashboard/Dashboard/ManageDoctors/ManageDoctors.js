2bdd89d0be4d32b12a3a73d71da51c580ec7363a
alimranhossain <alimranhossain44@gmail.com>
2022-11-20 12:33:20 +0600

    generics purpuse confirmation of modal

--- src/Pages/Dashboard/Dashboard/ManageDoctors/ManageDoctors.js
+++ src/Pages/Dashboard/Dashboard/ManageDoctors/ManageDoctors.js
@@ -1,28 +1,38 @@
-import { async } from '@firebase/util';
 import { useQuery } from '@tanstack/react-query';
-import React from 'react';
+import React, { useState } from 'react';
+import ConfirmationModal from '../../../Shared/ConfirmationModal/ConfirmationModal';
 import Loading from '../../../Shared/Loading/Loading';
 
 const ManageDoctors = () => {
-    const {data: doctors, isLoading} = useQuery({
+    const [deletingDoctor, setDeletingDoctor] = useState(null);
+
+    const closeModal = () =>{
+        setDeletingDoctor(null);
+    }
+
+    const handleDeleteDoctor = doctor =>{
+        console.log(doctor);
+    }
+
+    const { data: doctors, isLoading } = useQuery({
         queryKey: ['doctors'],
-        queryFn: async()=>{
-            try{
-                const res = await fetch('http://localhost:5000/doctors',{
-                    headers:{
+        queryFn: async () => {
+            try {
+                const res = await fetch('http://localhost:5000/doctors', {
+                    headers: {
                         authorization: `bearer ${localStorage.getItem('accessToken')}`
                     }
                 });
                 const data = await res.json();
                 return data;
             }
-            catch(error){
+            catch (error) {
 
             }
         }
     })
 
-    if(isLoading){
+    if (isLoading) {
         return <Loading></Loading>
     }
     return (
@@ -30,7 +40,7 @@ const ManageDoctors = () => {
             <h2 className='text-3xl'>Manage Doctors: {doctors?.length}</h2>
             <div className="overflow-x-auto">
                 <table className="table w-full">
-                 
+
                     <thead>
                         <tr>
                             <th></th>
@@ -49,7 +59,7 @@ const ManageDoctors = () => {
                                 <td>
                                     <div className="avatar">
                                         <div className="w-24 rounded-full">
-                                            <img src={doctor.image} alt=''/>
+                                            <img src={doctor.image} alt='' />
                                         </div>
                                     </div>
 
@@ -57,14 +67,27 @@ const ManageDoctors = () => {
                                 <td>{doctor.name}</td>
                                 <td>{doctor.email}</td>
                                 <td>{doctor.specialty}</td>
-                                <td><button className='btn btn-sm btn-error'>Delete</button></td>
+                                <td>
+                                    <label onClick={()=> setDeletingDoctor(doctor)} htmlFor="confirmation-modal" className="btn btn-sm btn-error">Delete</label>
+                                    
+                                </td>
                             </tr>)
                         }
-                      
-                       
+
+
                     </tbody>
                 </table>
             </div>
+            {
+                deletingDoctor && <ConfirmationModal 
+                    title={`Are you sure you want to delete?`}
+                    message={`If you delete ${deletingDoctor.name}. It cannot be undone`}
+                    successAction={handleDeleteDoctor}
+                    modalData = {deletingDoctor}
+                    closeModal={closeModal}
+                >
+                </ConfirmationModal>
+            }
         </div>
     );
 };
