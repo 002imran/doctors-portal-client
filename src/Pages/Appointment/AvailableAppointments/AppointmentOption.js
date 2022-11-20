e49254f328bccd81fa8334acd8da7c618f275b55
alimranhossain <alimranhossain44@gmail.com>
2022-11-20 16:55:25 +0600

    temporary update data

--- src/Pages/Appointment/AvailableAppointments/AppointmentOption.js
+++ src/Pages/Appointment/AvailableAppointments/AppointmentOption.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const AppointmentOption = ({appointmentOption, setTreatment}) => {
-    const {name, slots} = appointmentOption;
+    const {name, slots, price} = appointmentOption;
     return (
       <div className="card shadow-xl">
         <div className="card-body text-center mt-10">
@@ -12,6 +12,7 @@ const AppointmentOption = ({appointmentOption, setTreatment}) => {
           <p>
             {slots.length} {slots.length > 1 ? "spaces" : "space"} Available
           </p>
+          <p>Price: ${price}</p>
           <div className="card-actions justify-center">
            
             <label
